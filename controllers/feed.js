cc3ff5dcbd161c8346c680f3f96b0eb46fdefd61
Damian de Vega <damiandevega@gmail.com>
2020-07-22 09:50:26 -0400

    adding validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,1 +1,43 @@
-exports.getPosts = (req, res, next) => {};
+const { validationResult } = require('express-validator/check');
+
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        _id: '1',
+        title: 'First Post',
+        content: 'This is my first post.',
+        imageUrl: 'images/beach.jpeg',
+        creator: {
+          name: 'Damian',
+        },
+        createdAt: new Date(),
+      },
+    ],
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({
+        message: 'Validation failed, entered data is incorrect.',
+        errors: errors.array(),
+      });
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  // Create post in db
+  res.status(201).json({
+    message: 'Post created successfully',
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: 'Damian' },
+      createdAt: new Date(),
+    },
+  });
+};
