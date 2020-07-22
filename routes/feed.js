42777ef186d9d0d5605ebd087e6eb0adf669d526
Damian de Vega <damiandevega@gmail.com>
2020-07-22 16:18:33 -0400

    adding pagination

--- routes/feed.js
+++ routes/feed.js
@@ -18,4 +18,20 @@ router.post(
   feedController.createPost
 );
 
+// GET /feed/post/:postId
+router.get('/post/:postId', feedController.getPost);
+
+// PUT /feed/post/:postId
+router.put(
+  '/post/:postId',
+  [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 }),
+  ],
+  feedController.updatePost
+);
+
+// DELETE /feed/post/:postId
+router.delete('/post/:postId', feedController.deletePost);
+
 module.exports = router;
